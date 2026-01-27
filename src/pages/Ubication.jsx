b2e5a490ba2c97815a5f3266e9e25c6f3d4dcffe
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2026-01-27 14:25:22 +0100

    Component to Ubication done, and page for it too

--- src/pages/Ubication.jsx
+++ src/pages/Ubication.jsx
@@ -1,3 +1,8 @@
 function Ubication(){
-    
+    return(
+        <div class="ubication">
+            <h1>Ubicación</h1>
+            <iframe src="https://maps.app.goo.gl/vcCdCixhqtuFLa6H8"></iframe>
+        </div>
+    )
 }
